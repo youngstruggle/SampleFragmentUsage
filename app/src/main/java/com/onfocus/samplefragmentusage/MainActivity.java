c6da5342e5a970394638ad5ae54b29c3c73a615e
youngstruggle <febrian.nana@gmail.com>
2017-01-25 11:17:53 +0700

    Initial commit

---
+++ app/src/main/java/com/onfocus/samplefragmentusage/MainActivity.java
@@ -0,0 +1,21 @@
+package com.onfocus.samplefragmentusage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.frame_container,
+                            new FirstFragment(),
+                            FirstFragment.class.getSimpleName()).commit();
+        }
+    }
+}
+
